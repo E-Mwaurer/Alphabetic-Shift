fc051583cb320bacdfc4231b98c064e1ae2d8400
EdwardMwaura <edduemwaurer@outlook.com>
2019-08-26 18:23:56 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -4,12 +4,18 @@ import './style.css';
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>Alphabetic Shift</h1>`;
+
 function alphabeticShift(inputString: string): string {
-  let vals = inputString.split('');
-  // console.log(vals);
+  const alphabets = "abcdefghijklmnopqrstuvwxyza";
+  const charArr = [];
+  const vals = inputString.split('');
   for (let i = 0; i < vals.length; i++) {
-      console.log(vals[i]);
+    let nextVal = alphabets.indexOf(vals[i]);
+    charArr.push(alphabets.charAt(nextVal + 1));
   }
+  const finalStr = charArr.join("");
+  console.log(finalStr);
+  return finalStr;
 }
 
 console.log(alphabeticShift('crazy'));
