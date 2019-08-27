517b6b38f1c019085c1b0c2ef02f0ebde6d894e7
EdwardMwaura <edduemwaurer@outlook.com>
2019-08-27 11:30:09 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -6,16 +6,17 @@ const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>Alphabetic Shift</h1>`;
 
 function alphabeticShift(inputString: string): string {
-  const alphabets = "abcdefghijklmnopqrstuvwxyza";
-  const charArr = [];
   const vals = inputString.split('');
+  const alphabets = "abcdefghijklmnopqrstuvwxyz";
+  const charArr = [];
   for (let i = 0; i < vals.length; i++) {
     let nextVal = alphabets.indexOf(vals[i]);
+    if (alphabets.indexOf(vals[i]) === 25) {
+      nextVal = alphabets.indexOf('a') - 1;
+    }
     charArr.push(alphabets.charAt(nextVal + 1));
   }
-  const finalStr = charArr.join("");
-  console.log(finalStr);
-  return finalStr;
+  return charArr.join("");
 }
 
 console.log(alphabeticShift('crazy'));
